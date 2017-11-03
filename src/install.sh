8410a5132f667d49449f4a2fd44f3a6d60b4d81b
xutao <xuyz1988@163.com>
2017-11-03 11:41:48 +0800

    add install.sh

---
+++ src/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PWD=$(cd "$(dirname "$0")"; pwd)
+
+role=`id -u`
+if test $role -ne 0
+then
+    echo "Operation not permitted"
+    exit 1
+fi
+
+curl https://raw.githubusercontent.com/WALL-E/static/master/setup/redhat/install_python36|bash
+
+pip3.6 install -r requirements.txt
+
+command -v mysql
+ret=$?
+if test $ret -ne 0
+then
+    echo "install MySQL ......"
+    ${PWD}/misc/db/install.sh
+fi
